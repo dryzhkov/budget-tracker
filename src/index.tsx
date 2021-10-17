5125ccacd632454614493ceab0b524d7d17a6b68
Dmitry Ryzhkov <dimaryz@gmail.com>
2021-10-17 13:26:06 -0700

    added cra

--- src/index.tsx
+++ src/index.tsx
@@ -1,110 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useAuth0 } from '@auth0/auth0-react';
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
-
-import { setContext } from '@apollo/client/link/context';
-import { Categories } from './components/Categories';
-
-const httpLink = createHttpLink({
-  uri: 'https://graphql.us.fauna.com/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-// Bind to events
-const App = () => {
-  const {
-    loginWithRedirect,
-    logout,
-    getAccessTokenSilently,
-    isAuthenticated,
-    isLoading,
-    user,
-  } = useAuth0();
-
-  const handleSignoutClick = () => {
-    localStorage.removeItem('token');
-    logout();
-  };
-
-  const handleLoginClick = async () => {
-    loginWithRedirect();
-  };
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      if (isAuthenticated && !localStorage.getItem('token')) {
-        const token = await getAccessTokenSilently();
-        localStorage.setItem('token', token);
-      }
-    };
-
-    void fetchToken();
-  }, [isAuthenticated]);
-
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
-
-  return (
-    <>
-      <div>
-        {isAuthenticated && user ? (
-          <section>
-            <p>
-              Your token is: {JSON.stringify(localStorage.getItem('token'))}
-            </p>
-            <div>
-              <img src={user.picture} alt={user.name} />
-              <h2>{user.name}</h2>
-              <p>{user.email}</p>
-            </div>
-            <button onClick={handleSignoutClick}>Sign Out</button>
-
-            <Categories />
-          </section>
-        ) : (
-          <section>
-            <button onClick={handleLoginClick}>Sign In Here!</button>
-          </section>
-        )}
-      </div>
-    </>
-  );
-};
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dimaryz.auth0.com"
-    clientId="ehnorKu18hGNn09pl5MzItqPYciDl7UX"
-    audience="https://db.fauna.com/db/ytkuu7stcynrq"
-    redirectUri={window.location.origin}
-  >
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Auth0Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
