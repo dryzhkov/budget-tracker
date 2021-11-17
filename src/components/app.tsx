d7d706ba31960006daaab6e8abd953ed1b0a17e8
Dmitry Ryzhkov <dimaryz@gmail.com>
2021-11-16 22:02:08 -0800

    react router + navbar

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,57 +1,29 @@
-/** @jsxImportSource @emotion/react */
 import { ApolloProvider } from "../context/apolloProvider";
 import { useAuth } from "../context/authProvider";
 import { NavBar } from "./navBar";
-import React, { useState } from "react";
+import React from "react";
 import { FullPageSpinner } from "./lib";
-import { StatementDto, StatementPicker } from "./statementPicker";
-import { StatementEditor } from "./statementEditor";
-import { css } from "@emotion/react";
-
-const container = css`
-  padding: 10px;
-  display: flex;
-  height: calc(100vh - 50px);
-`;
-
-const left = css`
-  flex: 0 0 350px;
-  padding: 20px;
-`;
-const right = css`
-  flex: 1;
-  padding: 20px;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Categories } from "./categories";
+import { Budget } from "./budget";
 
 function App() {
   const { isAuthenticated, token } = useAuth();
-  const [selectedStatement, setSelectedStatement] =
-    useState<StatementDto | null>(null);
 
   return (
-    <React.Suspense fallback={<FullPageSpinner />}>
-      <NavBar />
-      {token && isAuthenticated ? (
-        <ApolloProvider>
-          <div css={container}>
-            <section css={left}>
-              <StatementPicker
-                statement={selectedStatement}
-                setSelectedStatement={setSelectedStatement}
-              />
-            </section>
-            <section css={right}>
-              {selectedStatement && (
-                <StatementEditor
-                  statement={selectedStatement}
-                  setSelectedStatement={setSelectedStatement}
-                />
-              )}
-            </section>
-          </div>
-        </ApolloProvider>
-      ) : null}
-    </React.Suspense>
+    <BrowserRouter>
+      <React.Suspense fallback={<FullPageSpinner />}>
+        <NavBar />
+        {token && isAuthenticated ? (
+          <ApolloProvider>
+            <Routes>
+              <Route path="/" element={<Budget />} />
+              <Route path="categories" element={<Categories />} />
+            </Routes>
+          </ApolloProvider>
+        ) : null}
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,15 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+
 import { useAuth } from "../context/authProvider";
 const styles = css({
-  backgroundColor: "#282c34",
-  minHeight: "50px",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: "calc(10px + 2vmin)",
-  color: "white",
+  paddingLeft: "20px",
 });
 export function NavBar() {
   const { logout, isAuthenticated, login } = useAuth();
@@ -19,12 +16,25 @@ export function NavBar() {
   const handleLoginClick = () => login();
 
   return (
-    <header css={styles}>
-      {!isAuthenticated ? (
-        <button onClick={handleLoginClick}>Sign In</button>
-      ) : (
-        <button onClick={handleSignoutClick}>Sign Out</button>
-      )}
-    </header>
+    <Navbar css={styles} collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        Budget Tracker
+      </Navbar.Brand>
+      <Nav>
+        {!isAuthenticated ? (
+          <Nav.Link onClick={handleLoginClick}>Sign in</Nav.Link>
+        ) : (
+          <>
+            <Nav.Link as={Link} to="/categories">
+              Categories
+            </Nav.Link>
+            <Nav.Link as={Link} to="/charts">
+              Charts
+            </Nav.Link>
+            <Nav.Link onClick={handleSignoutClick}>Sign out</Nav.Link>
+          </>
+        )}
+      </Nav>
+    </Navbar>
   );
 }
