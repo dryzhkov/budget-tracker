59b6806fb9de330daf81c97d1c982219e15e4fab
Dmitry Ryzhkov <dimaryz@gmail.com>
2024-01-04 13:17:29 -0800

    create invoices

--- app/models/invoice.ts
+++ app/models/invoice.ts
@@ -1,4 +1,4 @@
-import type { User } from "@prisma/client";
+import type { Invoice, User } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
@@ -15,5 +15,36 @@ export function getInvoices(userId: User["id"], state = "active") {
       category: true,
       frequency: true,
     },
+  });
+}
+
+export function createInvoice({
+  userId,
+  title,
+  category,
+  frequency,
+  state,
+  externalUrl,
+}: Omit<Invoice, "id" | "userId"> & {
+  userId: User["id"];
+}) {
+  return prisma.invoice.create({
+    data: {
+      userId,
+      title,
+      category,
+      frequency,
+      state,
+      externalUrl,
+    },
+  });
+}
+
+export function deleteInvoice({
+  id,
+  userId,
+}: Pick<Invoice, "id"> & { userId: User["id"] }) {
+  return prisma.invoice.deleteMany({
+    where: { id, userId },
   });
 }
--- app/routes/invoices.new.tsx
+++ app/routes/invoices.new.tsx
@@ -3,48 +3,72 @@ import { json, redirect } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
 import { useEffect, useRef } from "react";
 
-import { createStatement } from "~/models/statement";
+import { createInvoice } from "~/models/invoice";
 import { requireUserId } from "~/session.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const userId = await requireUserId(request);
   const formData = await request.formData();
-  const rawDate = formData.get("date");
 
-  if (typeof rawDate !== "string" || rawDate.length === 0) {
+  const title = formData.get("title");
+  if (typeof title !== "string" || title.length < 2) {
     return json(
-      { errors: { body: null, date: "Date is required" } },
+      { errors: { body: null, date: "Invalid title" } },
       { status: 400 },
     );
   }
 
-  const date = new Date(rawDate);
-
-  if (isNaN(date.getTime())) {
+  const category = formData.get("category");
+  if (typeof category !== "string" || category.length === 0) {
     return json(
-      { errors: { body: null, date: "Date must be valid" } },
+      { errors: { body: null, date: "Invalid category" } },
       { status: 400 },
     );
   }
 
-  const year = Number(date.getFullYear());
+  const frequency = formData.get("frequency");
+  if (typeof frequency !== "string" || frequency.length === 0) {
+    return json(
+      { errors: { body: null, date: "Invalid frequency" } },
+      { status: 400 },
+    );
+  }
 
-  await createStatement({
+  const state = formData.get("state");
+  if (typeof state !== "string" || state.length === 0) {
+    return json(
+      { errors: { body: null, date: "Invalid state" } },
+      { status: 400 },
+    );
+  }
+
+  const externalUrl = formData.get("url") ?? "";
+  if (typeof externalUrl !== "string" && externalUrl !== null) {
+    return json(
+      { errors: { body: null, date: "Invalid url" } },
+      { status: 400 },
+    );
+  }
+
+  await createInvoice({
     userId,
-    date,
-    year,
+    title,
+    category,
+    frequency,
+    state,
+    externalUrl,
   });
 
-  return redirect(".");
+  return redirect("/invoices");
 };
 
 export default function NewInvoicePage() {
   const actionData = useActionData<typeof action>();
-  const dateRef = useRef<HTMLInputElement>(null);
+  const titleRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (actionData?.errors?.date) {
-      dateRef.current?.focus();
+      titleRef.current?.focus();
     }
   }, [actionData]);
 
@@ -60,13 +84,14 @@ export default function NewInvoicePage() {
     >
       <div>
         <label className="flex w-full flex-col gap-1" htmlFor="datepicker">
-          <span>Select a date: </span>
+          <span>Title: </span>
           <input
-            type="date"
-            id="datepicker"
-            name="date"
-            className="max-w-32"
-            ref={dateRef}
+            type="text"
+            id="title"
+            name="title"
+            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            placeholder="Enter a title"
+            ref={titleRef}
             aria-invalid={actionData?.errors?.date ? true : undefined}
             aria-errormessage={
               actionData?.errors?.date ? "date-error" : undefined
@@ -78,6 +103,65 @@ export default function NewInvoicePage() {
             {actionData.errors.date}
           </div>
         ) : null}
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Category: </span>
+          <select
+            id="category"
+            name="category"
+            className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+            <option value="saving">Saving</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Frequency: </span>
+          <select
+            id="frequency"
+            name="frequency"
+            className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="bi-weekly">Bi Weekly</option>
+            <option value="monthly">Monthly</option>
+            <option value="bi-monthly">Bi Monthly</option>
+            <option value="quaterly">Quaterly</option>
+            <option value="one-off">One Off</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>State: </span>
+          <select
+            id="state"
+            name="state"
+            className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="active">Active</option>
+            <option value="archived">Archived</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>External Url: </span>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            placeholder="Enter a url"
+          />
+        </label>
       </div>
 
       <div className="text-right">
